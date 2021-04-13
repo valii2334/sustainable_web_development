147639c0d27e214c52088621519192621b56bf97
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2021-04-13 21:45:46 +0300

    CHORE: widgets index, show + routes

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -1,4 +1,19 @@
 class WidgetsController < ApplicationController
+  def show
+    @widget = OpenStruct.new(
+      id: params[:id],
+      manufacturer_id: rand(100),
+      name: "Widget #{params[:id]}"
+    )
+  end
+
+  def index
+    @widgets = [
+      OpenStruct.new(id: 1, name: 'Stembolt'),
+      OpenStruct.new(id: 2, name: 'Flux Capacitor')
+    ]
+  end
+
   def edit
     @widget = Widget.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :widgets, only: [:show, :index]
+  resources :widget_ratings, only: [:create]
+
+  # Custom routes start here
+  #
+  # For each custom route:
+  # * Be sure you have the cannonical route declared above
+  # * Add the new custom route below the existing ones
+  # * Document why it's needed
+  # * Explain anything else non-standard
+
+  # Used in podcast ads for the 'amazing' campaign
+  get '/amazing', to: redirect('/widgets')
 end
