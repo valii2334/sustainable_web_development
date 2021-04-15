342fdfc19b7a3065780e4968567da8a0c6727044
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2021-04-15 09:47:26 +0300

    CHORE: use helpers widgets#show'

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -15,6 +15,14 @@ class WidgetsController < ApplicationController
       manufacturer: manufacturer,
       name: "Widget #{params[:id]}"
     )
+
+    def @widget.widget_id
+      if self.id.to_s.length < 3
+        self.id.to_s
+      else
+        self.id.to_s[0..-3] + '.' + self.id.to_s[-2..-1]
+      end
+    end
   end
 
   def index
