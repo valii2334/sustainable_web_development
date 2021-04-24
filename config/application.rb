7164cc748728436cc0a7bcd67555b7db7ae5113c
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2021-04-24 09:48:38 +0300

    CHORE: default schema to sql, use postgres timestamptz

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,9 @@ module Widgets
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # We want to be able to use any features of our database
+    # and the SQL format make that possible
+    config.active_record.schema_format = :sql
   end
 end
